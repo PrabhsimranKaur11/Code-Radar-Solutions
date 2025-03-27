422af4a6d336fa252da5d42356ca2283b93b4a4a
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-03-28 00:39:12 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,7 +3,7 @@
 int fibbo(int n);
 int fibbo(int n){
     if(n==0){return 0;}
-    else{for(int i=0;i<=5;i++){printf(n + fibbo(n+1);)}
+    else{for(int i=0;i<=5;i++){printf("%d",n + fibbo(n+1);)}
 }}
 int main(){
     int n;
