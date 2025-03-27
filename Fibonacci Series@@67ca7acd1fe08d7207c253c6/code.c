4edde221b8f97b524352c5399305b8e9fd7ce9a4
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-03-28 01:06:42 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,9 +4,9 @@ int fibonacciSeries(int n){
     for(int i=0;i<n;i++){
          int f = 0;
             int s =1;
+            int t;
         if(n<=0){
-            int f = 0;
-            int s =1;f=t;}
+            f=t;}
         else{
             int t= f+s;
             f=s;
