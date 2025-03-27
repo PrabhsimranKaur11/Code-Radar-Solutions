33db1ffcf6288a361f67afa0a4fcea3db81aafdc
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-03-28 00:43:25 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,8 +3,8 @@
 int fibonacciSeries(int n);
 int fibonacciSeries(int n){
     if(n==0){return 0;}
-    else{for(int i=0;i<=5;i++){
+    else{for(int i=0;i<5;i++){
         int sum = 0;
-        sum +=i;
+        sum 
         printf("%d",sum );}
 }}
