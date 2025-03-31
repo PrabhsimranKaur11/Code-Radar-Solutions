9d3e6522fd20a15ff65e5d16071b10b5823d64a3
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-04-01 04:39:53 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
-int bubbleSort(int arr[], int n) {
- for (int i = 0; i < n - 1; i++) {
- for (int j = 0; j < n - i - 1; j++) {
- if (arr[j] > arr[j + 1]) {
- int temp = arr[j];
- arr[j] = arr[j + 1];
- arr[j + 1] = temp;
- }
- }
- }
+#include<stdio.h>
+int bubbleSort(int arr[],int n);
+int printArray(int arr[],int n);
+int bubbleSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            int temp;
+            if(arr[j]>arr[j+1]){
+                temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
 }
-int main() {
+int printArray(int arr[],int n){
+    for(int j =n;j>0;j--){
+        printf("%d ",arr[0]);
+    }
+}
+int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
- bubbleSort(arr, n);
- for (int i = 0; i < n; i++) {
-scanf("%d\n",&arr[i]);
- }
-  for (int i = 0; i < n; i++) {
-printf("%d ",arr[i]);}
- return 0;}
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    bubbleSort(arr,n);
+    printArray(arr,n);
+}
