8b720edce72259bb255383b9fc5c05da89f35c88
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-03-31 22:14:14 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factorialRange(int start,int end);
+int factorialRange(int start,int end){
+    for(int i = start;i<end;i++){
+        if(i=0 || i =1){
+            return 1;
+        }
+        else{
+            int product = 0;
+            product = product * i;
+            printf("%d",product);
+        }
+    }
+}
