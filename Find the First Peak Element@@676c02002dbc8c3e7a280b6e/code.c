7926341ad4095cc168e99dac75a002601f2f5134
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-04-01 04:28:48 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -17,6 +17,7 @@ int main() {
  bubbleSort(arr, n);
  for (int i = 0; i < n; i++) {
 scanf("%d\n",&arr[i]);
-printf("%d",arr[j]);
  }
+  for (int i = 0; i < n; i++) {
+printf("%d",&arr[i]);}
  return 0;}
