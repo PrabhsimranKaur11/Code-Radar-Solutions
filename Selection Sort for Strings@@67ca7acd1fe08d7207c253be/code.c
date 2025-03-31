6ddfdce6c7c91d6d87095eb93afec039522c6a15
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-04-01 00:35:20 +0530

    Solution to Selection Sort for Strings

---
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -0,0 +1,23 @@
+char[100] bubbleSort(char[100] arr[],int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+char[100] printArray(char[100] arr[], int n)
+{
+    for(int j=0;j<n;j++)
+    {
+        printf("%s ",arr[j]);
+    }
+}
