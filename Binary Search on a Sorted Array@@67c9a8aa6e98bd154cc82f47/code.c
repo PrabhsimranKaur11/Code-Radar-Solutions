596e3bad6aea87bc10082a687fe59c973054215d
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-04-01 11:29:56 +0530

    Solution to Binary Search on a Sorted Array

--- Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -3,11 +3,11 @@ int binarySearch(int arr,int n ,int target){
     int left=0;
     int right=n-1;
     while(left<=right){
-        int mid=left+(right-left)/2;
-if(int arr[mid]==target){
+        int mid=left+right/2;
+if(arr[mid]==target){
     return mid;
 }
-else if(int arr[mid]<target){
+else if(arr[mid]<target){
     left=mid+1;
 }
 else{
