212fbdc9b906c049918856479bfff7d7a2341fa1
PRABHSIMRAN KAUR <prabhkaur1902@gmail.com>
2025-04-01 10:55:07 +0530

    Solution to Binary Search on a Sorted Array

---
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -0,0 +1,17 @@
+int binarySearch(arr,n,target){
+    for(int i=0;i<n;i++){
+        found=0;
+         int result = i;
+        if{
+            arr[i]==target;
+            found=1;
+        }
+        else{found=0;}
+    }
+    if(found){
+        printf("%d",result);
+    }
+    else{
+        printf("-1");
+    }
+}
